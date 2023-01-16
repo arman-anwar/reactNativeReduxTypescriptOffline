7e202a52b0cf68f20a08f110cfe625d65b02af4b
Arman Anwar <arman.anwar@gmail.com>
2023-01-16 15:42:03 +0000

    api url fix

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -17,20 +17,20 @@ export const fetchUserAPI = (id) => {
 };
 
 export const createUserAPI = (data) => {
-  const url = `${BASE_URL}//user`;
+  const url = `${BASE_URL}/user`;
   const config = getConfig();
   console.log('Request.ppost >> ', data)
   return Request.post({ url, config, data });
 };
 
 export const updateUserAPI = (data) => {
-  const url = `${BASE_URL}//user/${data.id}`;
+  const url = `${BASE_URL}/user/${data.id}`;
   const config = getConfig();
   return Request.put({ url, config, data: { name: data.name, email: data.email } });
 };
 
 export const deleteUserAPI = (data) => {
-  const url = `${BASE_URL}//user/${data}`;
+  const url = `${BASE_URL}/user/${data}`;
   const config = getConfig();
   return Request.del({ url, config });
 };
